c704ea9bf8e60c3e3e45eb3a6f0261722a753ab4
wtupc96 <594373559@qq.com>
2017-01-05 16:22:52 +0800

    Basic functions with a few bugs.

--- app/src/main/java/com/ouyang/peter/myapp/MainActivity.java
+++ app/src/main/java/com/ouyang/peter/myapp/MainActivity.java
@@ -1,9 +1,11 @@
 package com.ouyang.peter.myapp;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.KeyEvent;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -18,12 +20,25 @@ import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
 public class MainActivity extends AppCompatActivity {
+    private static final int UNI_VERIFIED = 2131492957;
+    private static final int LIBRARY = 2131492958;
+    private static final int FLOW = 2131492959;
+
+    private static final String USERNAMEFORUNI = "usernameForUni";
+    private static final String PASSWORDFORUNI = "passwordForUni";
+    private static final String USERNAMEFORLIBRARY = "usernameForLibrary";
+    private static final String PASSWORDFORLIBRARY = "passwordForLibrary";
+    private static final String USERNAMEFORFLOW = "usernameForFlow";
+    private static final String PASSWORDFORFLOW = "passwordForFlow";
+
+    private static final int REQ_CODE_0 = 0;
+    private static final String SETTING_INFOS = "Setting_infos";
 
     private static final String[] strs = new String[]{"选课端", "学生端", "缴费", "教务处", "出勤情况", "图书信息查询", "借阅查询", "流量查询", "网络导航"};
     private static String[] urls = new String[]{"https://cas.xjtu.edu.cn/login?service=http%3A%2F%2Fxkfw.xjtu.edu.cn%2Fxsxk%2Flogin.xk", "https://cas.xjtu.edu.cn/login?service=http%3A%2F%2Fssfw.xjtu.edu.cn%2Findex.portal", "https://cas.xjtu.edu.cn/login?service=http://card.xjtu.edu.cn:8050/Account/CASSignIn", "http://dean.xjtu.edu.cn", "http://202.117.1.152:8080/user", "http://202.117.24.14/", "http://202.117.24.14/patroninfo*chx", "http://auth.xjtu.edu.cn/default.aspx", "http://xjtu.so/"};
     private static String url = urls[3];
+    private static int positionUrl = 3;
 
-    private static CookieManager cookieManager;
     private myDialog myDialog = new myDialog();
     private static ListView listView;
     private static WebView webView;
@@ -34,28 +49,12 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        setCookieManager();
-
         setWebView();
 
         webView.loadUrl(url);
 
         setListView();
 
-    }
-
-    private void setCookieManager() {
-        cookieManager = CookieManager.getInstance();
-        cookieManager.setAcceptCookie(true);
-    }
-
-    @Override
-    public boolean onKeyDown(int keyCode, KeyEvent event) {
-        if (event.getKeyCode() == KeyEvent.KEYCODE_BACK && webView.canGoBack())
-            webView.goBack();
-        else
-            super.onKeyDown(keyCode, event);
-        return true;
     }
 
     private void setListView() {
@@ -67,7 +66,7 @@ public class MainActivity extends AppCompatActivity {
         listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-                webView.loadUrl(url = urls[position]);
+                webView.loadUrl(url = urls[positionUrl = position]);
             }
         });
     }
@@ -107,6 +106,34 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onPageFinished(WebView view, String url) {
                 super.onPageFinished(view, url);
+                SharedPreferences sharedPreferences = getSharedPreferences(SETTING_INFOS, 0);
+                switch (positionUrl) {
+                    case 0:
+                    case 1:
+                    case 2:
+                    case 4:
+                        view.loadUrl("JavaScript: {" +
+                                "document.getElementById('username').value = '" + sharedPreferences.getString(USERNAMEFORUNI, "") + "';" +
+                                "document.getElementById('password').value = '" + sharedPreferences.getString(PASSWORDFORUNI, "") + "';" +
+                                "var frms = document.getElementsById('fm1');" +
+                                "frms[0].submit(); };");
+                        break;
+                    case 6:
+                        view.loadUrl("JavaScript: {" +
+                                "document.getElementById('code').value = '" + sharedPreferences.getString(USERNAMEFORLIBRARY, "") + "';" +
+                                "document.getElementById('pin').value = '" + sharedPreferences.getString(PASSWORDFORLIBRARY, "") + "';" +
+                                "var frms = document.getElementsById('fm1');" +
+                                "frms[0].submit(); };");
+                        break;
+                    case 7:
+                        view.loadUrl("JavaScript: {" +
+                                "document.getElementById('TB_userName').value = '" + sharedPreferences.getString(USERNAMEFORFLOW, "") + "';" +
+                                "document.getElementById('TB_password').value = '" + sharedPreferences.getString(PASSWORDFORFLOW, "") + "';" +
+                                "var frms = document.getElementsById('fm1');" +
+                                "frms[0].submit(); };");
+                        break;
+
+                }
 
             }
         });
@@ -125,6 +152,59 @@ public class MainActivity extends AppCompatActivity {
                 super.onProgressChanged(view, newProgress);
             }
         });
+    }
+
+    @Override
+    public boolean onKeyDown(int keyCode, KeyEvent event) {
+        if (event.getKeyCode() == KeyEvent.KEYCODE_BACK && webView.canGoBack())
+            webView.goBack();
+        else
+            super.onKeyDown(keyCode, event);
+        return true;
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        try {
+            switch (resultCode) {
+                case REQ_CODE_0:
+                    Bundle bundle = data.getBundleExtra("userInform");
+                    int type = bundle.getInt("type");
+                    String usernameString = "";
+                    String passwordString = "";
+                    switch (type) {
+                        case UNI_VERIFIED:
+                            usernameString = USERNAMEFORUNI;
+                            passwordString = PASSWORDFORUNI;
+                            break;
+                        case LIBRARY:
+                            usernameString = USERNAMEFORLIBRARY;
+                            passwordString = PASSWORDFORLIBRARY;
+                            break;
+                        case FLOW:
+                            usernameString = USERNAMEFORFLOW;
+                            passwordString = PASSWORDFORFLOW;
+                            break;
+                        default:
+                            try {
+                                throw new Exception("No responding type");
+                            } catch (Exception e) {
+                                e.printStackTrace();
+                            }
+                    }
+                    SharedPreferences sharedPreferences = getSharedPreferences(SETTING_INFOS, 0);
+                    sharedPreferences.edit()
+                            .putString(usernameString, bundle.getString(usernameString))
+                            .putString(passwordString, bundle.getString(passwordString))
+                            .commit();
+                    return;
+                default:
+                    return;
+            }
+        } catch (Exception e) {
+            Log.e("ExceptionOccurred", e.toString());
+        }
     }
 
     @Override
@@ -148,7 +228,7 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse(webView.getUrl())));
                 return true;
             case 2:
-                startActivity(new Intent("com.peter.ouyang.formInformationActivity"));
+                startActivityForResult(new Intent("com.peter.ouyang.formInformationActivity"), REQ_CODE_0);
                 return true;
         }
         return false;
@@ -157,8 +237,10 @@ public class MainActivity extends AppCompatActivity {
     private void createMenu(Menu menu) {
         MenuItem refresh = menu.add(0, 0, 0, "刷新");
         refresh.setShowAsAction(MenuItem.SHOW_AS_ACTION_NEVER);
+
         MenuItem openInBrowser = menu.add(0, 1, 1, "在浏览器中打开");
         openInBrowser.setShowAsAction(MenuItem.SHOW_AS_ACTION_NEVER);
+
         MenuItem addInform = menu.add(1, 2, 2, "管理信息");
         addInform.setShowAsAction(MenuItem.SHOW_AS_ACTION_NEVER);
     }
--- app/src/main/java/com/ouyang/peter/myapp/formInformation.java
+++ app/src/main/java/com/ouyang/peter/myapp/formInformation.java
@@ -1,11 +1,15 @@
 package com.ouyang.peter.myapp;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
+import android.view.KeyEvent;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.RadioGroup;
 import android.widget.Toast;
 
 import java.io.BufferedReader;
@@ -21,18 +25,58 @@ import java.io.OutputStreamWriter;
  * Created by wtupc96 on 2017/1/3.
  */
 public class formInformation extends AppCompatActivity {
+
+    private static final int RES_CODE_0 = 0;
+
+    private static final int UNI_VERIFIED = 2131492957;
+    private static final int LIBRARY = 2131492958;
+    private static final int FLOW = 2131492959;
+    private static final String USERNAMEFORUNI = "usernameForUni";
+    private static final String PASSWORDFORUNI = "passwordForUni";
+    private static final String USERNAMEFORLIBRARY = "usernameForLibrary";
+    private static final String PASSWORDFORLIBRARY = "passwordForLibrary";
+    private static final String USERNAMEFORFLOW = "usernameForFlow";
+    private static final String PASSWORDFORFLOW = "passwordForFlow";
+
+    private static RadioGroup radioGroup;
     private static EditText username;
     private static EditText password;
+    private static Button btn_forget;
     private static Button btn_submit;
+
+    private static String usernameString;
+    private static String passwordString;
+    private static int type = UNI_VERIFIED;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.form_layout);
 
+        radioGroup = (RadioGroup) findViewById(R.id.radioGroup);
+        btn_forget = (Button) findViewById(R.id.forgetInform);
         username = (EditText) findViewById(R.id.username);
         password = (EditText) findViewById(R.id.password);
         btn_submit = (Button) findViewById(R.id.submitInform);
+
+        radioGroup.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(RadioGroup group, int checkedId) {
+                switch (type = checkedId) {
+                    case UNI_VERIFIED:
+                        usernameString = USERNAMEFORUNI;
+                        passwordString = PASSWORDFORUNI;
+                        return;
+                    case LIBRARY:
+                        usernameString = USERNAMEFORLIBRARY;
+                        passwordString = PASSWORDFORLIBRARY;
+                        return;
+                    case FLOW:
+                        usernameString = USERNAMEFORFLOW;
+                        passwordString = PASSWORDFORFLOW;
+                }
+            }
+        });
 
         btn_submit.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -42,52 +86,49 @@ public class formInformation extends AppCompatActivity {
                     return;
                 }
 
-//                try (FileWriter fileWriter = new FileWriter(new File("Information.peter"))) {
-//                    fileWriter.write(username.getText().toString().toCharArray());
-//                    fileWriter.write('\n');
-//                    fileWriter.write(password.getText().toString().toCharArray());
-//                    fileWriter.flush();
-//                } catch (FileNotFoundException e) {
-//                    e.printStackTrace();
-//                } catch (IOException e) {
-//                    e.printStackTrace();
-//                }
+                Bundle bundle = new Bundle();
+                bundle.putString(usernameString, username.getText().toString());
+                bundle.putString(passwordString, password.getText().toString());
 
-                try {
-                    FileOutputStream fileOutputStream = openFileOutput("textFile.txt", MODE_PRIVATE);
-                    OutputStreamWriter outputStreamWriter = new OutputStreamWriter(fileOutputStream);
+                Log.e("tttt", String.valueOf(type));
 
-                    outputStreamWriter.write(username.getText().toString());
-                    outputStreamWriter.write("\n");
-                    outputStreamWriter.write(password.getText().toString());
+                bundle.putInt("type", type);
 
-                    outputStreamWriter.flush();
-                    outputStreamWriter.close();
+                Intent intent = new Intent();
+                intent.putExtra("userInform", bundle);
 
-                    Toast.makeText(formInformation.this, "saved", Toast.LENGTH_SHORT).show();
-                } catch (FileNotFoundException e) {
-                    Log.e("exception", "File not Found occurs~~~~~~~~~");
-                    e.printStackTrace();
-                } catch (IOException e) {
-                    Log.e("exception", "IOException occurs~~~~~~~~~");
-                    e.printStackTrace();
-                }
+                setResult(RES_CODE_0, intent);
+
+                Toast.makeText(formInformation.this, "用户名和密码已保存~", Toast.LENGTH_SHORT).show();
+
+                finish();
+            }
+        });
+
+        btn_forget.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.e("yyyy", String.valueOf(type));
+
+                Bundle bundle = new Bundle();
+                bundle.putString(usernameString, "");
+                bundle.putString(passwordString, "");
+                bundle.putInt("type", type);
+
+                Intent intent = new Intent();
+                intent.putExtra("userInform", bundle);
+
+                setResult(RES_CODE_0, intent);
+
+                Toast.makeText(formInformation.this, "用户名和密码已清空~", Toast.LENGTH_SHORT).show();
             }
         });
     }
 
-    private void informTest() {
-
-        try (BufferedReader bufferedReader = new BufferedReader(new FileReader(new File("Information.peter")))) {
-            String dataLine;
-            while ((dataLine = bufferedReader.readLine()) != null)
-                Log.e("msg", dataLine);
-        } catch (FileNotFoundException e) {
-            Log.e("exception", "File not Found occurs~~~~~~~~~");
-            e.printStackTrace();
-        } catch (IOException e) {
-            Log.e("exception", "IOException occurs~~~~~~~~~");
-            e.printStackTrace();
-        }
+    @Override
+    public boolean onKeyDown(int keyCode, KeyEvent event) {
+        if (event.getKeyCode() == KeyEvent.KEYCODE_BACK)
+            finish();
+        return super.onKeyDown(keyCode, event);
     }
 }

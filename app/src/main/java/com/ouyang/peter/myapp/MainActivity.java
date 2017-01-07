ba01456ed63ed59088c2bd59b9b985286de7faa6
wtupc96 <594373559@qq.com>
2017-01-07 17:04:08 +0800

    Code is cleaned.

--- app/src/main/java/com/ouyang/peter/myapp/MainActivity.java
+++ app/src/main/java/com/ouyang/peter/myapp/MainActivity.java
@@ -10,7 +10,6 @@ import android.view.KeyEvent;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
-import android.webkit.CookieManager;
 import android.webkit.WebChromeClient;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
@@ -20,49 +19,74 @@ import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
 public class MainActivity extends AppCompatActivity {
+    // 用于确认由form传回信息的类型
     private static final int UNI_VERIFIED = 2131492957;
     private static final int LIBRARY = 2131492958;
     private static final int FLOW = 2131492959;
 
-    private static final String USERNAMEFORUNI = "usernameForUni";
-    private static final String PASSWORDFORUNI = "passwordForUni";
-    private static final String USERNAMEFORLIBRARY = "usernameForLibrary";
-    private static final String PASSWORDFORLIBRARY = "passwordForLibrary";
-    private static final String USERNAMEFORFLOW = "usernameForFlow";
-    private static final String PASSWORDFORFLOW = "passwordForFlow";
+    // 用于确认由form表单传回信息的标识（根据上述三条）
+    private static final String USERNAME_FOR_UNI = "usernameForUni";
+    private static final String PASSWORD_FOR_UNI = "passwordForUni";
+    private static final String USERNAME_FOR_LIBRARY = "usernameForLibrary";
+    private static final String PASSWORD_FOR_LIBRARY = "passwordForLibrary";
+    private static final String USERNAME_FOR_FLOW = "usernameForFlow";
+    private static final String PASSWORD_FOR_FLOW = "passwordForFlow";
+    //表单id信息
+    private static final String UNI_USERNAME_ID = "username";
+    private static final String UNI_PASSWORD_ID = "password";
+    private static final String LIBRARY_USERNAME_ID = "code";
+    private static final String LIBRARY_PASSWORD_ID = "pin";
+    private static final String FLOW_USERNAME_ID = "TB_userName";
+    private static final String FLOW_PASSWORD_ID = "TB_password";
 
+    // 存储类别信息
+    private static String usernameString;
+    private static String passwordString;
+    // 存储表单id信息
+    private static String usernameId;
+    private static String passwordId;
+
+    // 由form表单传回的结果号
     private static final int REQ_CODE_0 = 0;
+    // 存储文件的名称
     private static final String SETTING_INFOS = "Setting_infos";
 
+    // 抽屉中的提示信息
     private static final String[] strs = new String[]{"选课端", "学生端", "缴费", "教务处", "出勤情况", "图书信息查询", "借阅查询", "流量查询", "网络导航"};
+    // 对应的网址
     private static String[] urls = new String[]{"https://cas.xjtu.edu.cn/login?service=http%3A%2F%2Fxkfw.xjtu.edu.cn%2Fxsxk%2Flogin.xk", "https://cas.xjtu.edu.cn/login?service=http%3A%2F%2Fssfw.xjtu.edu.cn%2Findex.portal", "https://cas.xjtu.edu.cn/login?service=http://card.xjtu.edu.cn:8050/Account/CASSignIn", "http://dean.xjtu.edu.cn", "http://202.117.1.152:8080/user", "http://202.117.24.14/", "http://202.117.24.14/patroninfo*chx", "http://auth.xjtu.edu.cn/default.aspx", "http://xjtu.so/"};
+    // 应用初始化为教务处网址
     private static String url = urls[3];
+    // 标识当前webView装载的页面的下标，用户load对应Javascript
     private static int positionUrl = 3;
 
+    // 进度弹框对象
     private myDialog myDialog = new myDialog();
+    // 界面控件对象
     private static ListView listView;
     private static WebView webView;
+    // 标识webView装载的Progress
     private static boolean webViewFlag = true;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+        // 设置webView相关属性
         setWebView();
-
-        webView.loadUrl(url);
-
+        // 设置抽屉选项相关属性
         setListView();
-
     }
 
     private void setListView() {
+        // 获取ListView对象
         listView = (ListView) findViewById(R.id.id_lv);
 
+        // 配置适配器
         ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, strs);
         listView.setAdapter(arrayAdapter);
 
+        // 点击listView时页面装载相应网页
         listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
@@ -72,68 +96,84 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private void setWebView() {
-
+        // 获取webView控件对象
         webView = (WebView) findViewById(R.id.webView);
 
+        // 获取webView设置
         WebSettings webSettings = webView.getSettings();
 
+        // 设置为从本地读取网页否则从网络抓取
         webSettings.setCacheMode(WebSettings.LOAD_CACHE_ELSE_NETWORK);
+        // 设置为允许应用缓存
         webSettings.setAppCacheEnabled(true);
+        // 设置为网页允许缩放
         webSettings.setBuiltInZoomControls(true);
-        webSettings.setLayoutAlgorithm(WebSettings.LayoutAlgorithm.NARROW_COLUMNS);
-        webSettings.setLoadWithOverviewMode(true);
-        webSettings.setDisplayZoomControls(true);
+        // 允许Javascript打开窗口
         webSettings.setJavaScriptCanOpenWindowsAutomatically(true);
+        // 允许读取地理位置
         webSettings.setGeolocationEnabled(true);
+        // 允许页面的Javascript
         webSettings.setJavaScriptEnabled(true);
+        // 允许页面自动加载图片
         webSettings.setLoadsImagesAutomatically(true);
+        // 允许页面存储DOM树
         webSettings.setDomStorageEnabled(true);
+        // 允许页面保存表单信息
         webSettings.setSaveFormData(true);
+        // 支持多窗口
         webSettings.setSupportMultipleWindows(true);
 
         //解决了显示错位问题
         webSettings.setUseWideViewPort(true);
 
+        // 允许缩放
         webSettings.setSupportZoom(true);
 
         webView.setWebViewClient(new WebViewClient() {
+            // 页面跳转时保证在当前webView内跳转
             @Override
             public boolean shouldOverrideUrlLoading(WebView view, String url) {
                 view.loadUrl(url);
                 return true;
             }
 
+            // 页面加载完成时加载相应自动填充表单的Javascript信息
             @Override
             public void onPageFinished(WebView view, String url) {
                 super.onPageFinished(view, url);
                 SharedPreferences sharedPreferences = getSharedPreferences(SETTING_INFOS, 0);
                 switch (positionUrl) {
+                    // 统一身份认证
                     case 0:
                     case 1:
                     case 2:
                     case 4:
-                        view.loadUrl("JavaScript: {" +
-                                "document.getElementById('username').value = '" + sharedPreferences.getString(USERNAMEFORUNI, "") + "';" +
-                                "document.getElementById('password').value = '" + sharedPreferences.getString(PASSWORDFORUNI, "") + "';" +
-                                "var frms = document.getElementsById('fm1');" +
-                                "frms[0].submit(); };");
+                        usernameString = USERNAME_FOR_UNI;
+                        passwordString = PASSWORD_FOR_UNI;
+                        usernameId = UNI_USERNAME_ID;
+                        passwordId = UNI_PASSWORD_ID;
                         break;
+                    // 图书借阅
                     case 6:
-                        view.loadUrl("JavaScript: {" +
-                                "document.getElementById('code').value = '" + sharedPreferences.getString(USERNAMEFORLIBRARY, "") + "';" +
-                                "document.getElementById('pin').value = '" + sharedPreferences.getString(PASSWORDFORLIBRARY, "") + "';" +
-                                "var frms = document.getElementsById('fm1');" +
-                                "frms[0].submit(); };");
+                        usernameString = USERNAME_FOR_LIBRARY;
+                        passwordString = PASSWORD_FOR_LIBRARY;
+                        usernameId = LIBRARY_USERNAME_ID;
+                        passwordId = LIBRARY_PASSWORD_ID;
                         break;
+                    // 流量查询
                     case 7:
-                        view.loadUrl("JavaScript: {" +
-                                "document.getElementById('TB_userName').value = '" + sharedPreferences.getString(USERNAMEFORFLOW, "") + "';" +
-                                "document.getElementById('TB_password').value = '" + sharedPreferences.getString(PASSWORDFORFLOW, "") + "';" +
-                                "var frms = document.getElementsById('fm1');" +
-                                "frms[0].submit(); };");
+                        usernameString = USERNAME_FOR_FLOW;
+                        passwordString = PASSWORD_FOR_FLOW;
+                        usernameId = FLOW_USERNAME_ID;
+                        passwordId = FLOW_PASSWORD_ID;
                         break;
 
                 }
+                view.loadUrl("JavaScript: {" +
+                        "document.getElementById('" + usernameId + "').value = '" + sharedPreferences.getString(usernameString, "") + "';" +
+                        "document.getElementById('" + passwordId + "').value = '" + sharedPreferences.getString(passwordString, "") + "';" +
+                        "var frms = document.getElementsById('fm1');" +
+                        "frms[0].submit(); };");
 
             }
         });
@@ -152,6 +192,8 @@ public class MainActivity extends AppCompatActivity {
                 super.onProgressChanged(view, newProgress);
             }
         });
+
+        webView.loadUrl(url);
     }
 
     @Override
@@ -175,16 +217,16 @@ public class MainActivity extends AppCompatActivity {
                     String passwordString = "";
                     switch (type) {
                         case UNI_VERIFIED:
-                            usernameString = USERNAMEFORUNI;
-                            passwordString = PASSWORDFORUNI;
+                            usernameString = USERNAME_FOR_UNI;
+                            passwordString = PASSWORD_FOR_UNI;
                             break;
                         case LIBRARY:
-                            usernameString = USERNAMEFORLIBRARY;
-                            passwordString = PASSWORDFORLIBRARY;
+                            usernameString = USERNAME_FOR_LIBRARY;
+                            passwordString = PASSWORD_FOR_LIBRARY;
                             break;
                         case FLOW:
-                            usernameString = USERNAMEFORFLOW;
-                            passwordString = PASSWORDFORFLOW;
+                            usernameString = USERNAME_FOR_FLOW;
+                            passwordString = PASSWORD_FOR_FLOW;
                             break;
                         default:
                             try {
--- app/src/main/java/com/ouyang/peter/myapp/formInformation.java
+++ app/src/main/java/com/ouyang/peter/myapp/formInformation.java
@@ -2,9 +2,7 @@ package com.ouyang.peter.myapp;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
-import android.util.Log;
 import android.view.KeyEvent;
 import android.view.View;
 import android.widget.Button;
@@ -12,38 +10,34 @@ import android.widget.EditText;
 import android.widget.RadioGroup;
 import android.widget.Toast;
 
-import java.io.BufferedReader;
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.FileOutputStream;
-import java.io.FileReader;
-import java.io.FileWriter;
-import java.io.IOException;
-import java.io.OutputStreamWriter;
-
 /**
  * Created by wtupc96 on 2017/1/3.
  */
 public class formInformation extends AppCompatActivity {
 
+    // 返回给MainActivity的标识号
     private static final int RES_CODE_0 = 0;
 
+    // 三个RadioButton的id
     private static final int UNI_VERIFIED = 2131492957;
     private static final int LIBRARY = 2131492958;
     private static final int FLOW = 2131492959;
-    private static final String USERNAMEFORUNI = "usernameForUni";
-    private static final String PASSWORDFORUNI = "passwordForUni";
-    private static final String USERNAMEFORLIBRARY = "usernameForLibrary";
-    private static final String PASSWORDFORLIBRARY = "passwordForLibrary";
-    private static final String USERNAMEFORFLOW = "usernameForFlow";
-    private static final String PASSWORDFORFLOW = "passwordForFlow";
+    // 选择RadioButton的标识
+    private static final String USERNAME_FOR_UNI = "usernameForUni";
+    private static final String PASSWORD_FOR_UNI = "passwordForUni";
+    private static final String USERNAME_FOR_LIBRARY = "usernameForLibrary";
+    private static final String PASSWORD_FOR_LIBRARY = "passwordForLibrary";
+    private static final String USERNAME_FOR_FLOW = "usernameForFlow";
+    private static final String PASSWORD_FOR_FLOW = "passwordForFlow";
 
+    // 获取界面控件对象
     private static RadioGroup radioGroup;
     private static EditText username;
     private static EditText password;
     private static Button btn_forget;
     private static Button btn_submit;
 
+    // 保存RadioButton的标识值
     private static String usernameString;
     private static String passwordString;
     private static int type;
@@ -52,9 +46,10 @@ public class formInformation extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
+        // 初始化
         type = UNI_VERIFIED;
-        usernameString = USERNAMEFORUNI;
-        passwordString = PASSWORDFORUNI;
+        usernameString = USERNAME_FOR_UNI;
+        passwordString = PASSWORD_FOR_UNI;
 
         setContentView(R.layout.form_layout);
 
@@ -64,39 +59,42 @@ public class formInformation extends AppCompatActivity {
         password = (EditText) findViewById(R.id.password);
         btn_submit = (Button) findViewById(R.id.submitInform);
 
+        // RadioGroup设置状态改变监听器，根据所选的RadioButton来设置返回标识值
         radioGroup.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
             @Override
             public void onCheckedChanged(RadioGroup group, int checkedId) {
                 switch (type = checkedId) {
                     case UNI_VERIFIED:
-                        usernameString = USERNAMEFORUNI;
-                        passwordString = PASSWORDFORUNI;
+                        usernameString = USERNAME_FOR_UNI;
+                        passwordString = PASSWORD_FOR_UNI;
                         return;
                     case LIBRARY:
-                        usernameString = USERNAMEFORLIBRARY;
-                        passwordString = PASSWORDFORLIBRARY;
+                        usernameString = USERNAME_FOR_LIBRARY;
+                        passwordString = PASSWORD_FOR_LIBRARY;
                         return;
                     case FLOW:
-                        usernameString = USERNAMEFORFLOW;
-                        passwordString = PASSWORDFORFLOW;
+                        usernameString = USERNAME_FOR_FLOW;
+                        passwordString = PASSWORD_FOR_FLOW;
                 }
             }
         });
 
+        // submit按钮监听器
         btn_submit.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                // 确保用户名和密码都被填写
                 if (username.getText().toString().equals("") || password.getText().toString().equals("")) {
                     Toast.makeText(formInformation.this, "请输入用户名和密码~", Toast.LENGTH_SHORT).show();
                     return;
                 }
 
+                // 准备回传给MainActivity的数据包
                 Bundle bundle = new Bundle();
                 bundle.putString(usernameString, username.getText().toString());
                 bundle.putString(passwordString, password.getText().toString());
 
-                Log.e("tttt", String.valueOf(type));
-
+                // 表明传回的值是针对哪个类型页面的
                 bundle.putInt("type", type);
 
                 Intent intent = new Intent();
@@ -104,17 +102,19 @@ public class formInformation extends AppCompatActivity {
 
                 setResult(RES_CODE_0, intent);
 
+                // 用户提示信息
                 Toast.makeText(formInformation.this, "用户名和密码已保存~", Toast.LENGTH_SHORT).show();
 
+                // 结束当前Activity
                 finish();
             }
         });
 
+        // 忘记密码按钮的监听器
         btn_forget.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Log.e("yyyy", String.valueOf(type));
-
+                //将传回Activity的数据置为“”即可达到重置效果
                 Bundle bundle = new Bundle();
                 bundle.putString(usernameString, "");
                 bundle.putString(passwordString, "");
@@ -125,11 +125,13 @@ public class formInformation extends AppCompatActivity {
 
                 setResult(RES_CODE_0, intent);
 
+                // 用户提示信息
                 Toast.makeText(formInformation.this, "用户名和密码已清空~", Toast.LENGTH_SHORT).show();
             }
         });
     }
 
+    // 按下返回键时结束当前Activity
     @Override
     public boolean onKeyDown(int keyCode, KeyEvent event) {
         if (event.getKeyCode() == KeyEvent.KEYCODE_BACK)
